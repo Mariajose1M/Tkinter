f1ec95430937944094762f5283c0f3de11fae665
Maria Jose <lopezalvaradomariajosee6.5@gmail.com>
2025-10-16 17:42:05 -0500

    Actividad

--- Prueb_tkinter.py
+++ Prueb_tkinter.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 def sumar():
     try:
         n1 = float(entrada1.get())
@@ -5,21 +7,26 @@ def sumar():
         resultado.set(n1 + n2)
     except ValueError:
         resultado.set("Error")
+
 #--------------------Ventana principal-------------------------------#
-ventana= tk.Tk()
-ventana.title ("Calculadora basica")
+ventana = tk.Tk()
+ventana.title("Calculadora basica")
 ventana.geometry("250x180")
+
 #--------------------Variables---------------------------------------#
-resultado=tk.stringvar()
+resultado = tk.StringVar()
+
 #----------------------Widgets---------------------------------------#
-tk.Label(ventana, text="Numero 1:"). pack(pady=5)
-entrada1=tk.entry(ventana)
+tk.Label(ventana, text="Numero 1:").pack(pady=5)
+entrada1 = tk.Entry(ventana)
 entrada1.pack()
 
-tk.Label (ventana, text="numero2:").pack (pady=5)
-entrada2=tk.entry(ventana)
+tk.Label(ventana, text="Numero 2:").pack(pady=5)
+entrada2 = tk.Entry(ventana)
 entrada2.pack()
 
-tk.button (ventana, text="sumar", command=suma).pack(pady=5)
-tk.label (ventana,text="resultado:").pack(tk.Label(ventana,textvariable=resultado,fg="pink",font=("Arial",12)).pack()
-    ventana.mainloop()
+tk.Button(ventana, text="Sumar", command=sumar).pack(pady=5)
+tk.Label(ventana, text="Resultado:").pack()
+tk.Label(ventana, textvariable=resultado, fg="pink", font=("Arial", 12)).pack()
+
+ventana.mainloop()
